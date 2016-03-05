c2513db3c5eb8f82c12f366c72f2efb83fee6379
Peter Aronoff <telemachus@arpinum.org>
2016-03-05 12:04:53 -0500

    Tests for iterator

--- test/test-iterator.lua
+++ test/test-iterator.lua
@@ -3,7 +3,6 @@ package.path = '../src/?.lua;' .. package.path
 local spliter = require 'split'.spliterator
 local t = require 'tapered'
 
-
 local str = 'a,foo,bar,bizz,buzz,,b'
 local expected = { 'a', 'foo', 'bar', 'bizz', 'buzz', '', 'b' }
 local actual = {}
@@ -11,6 +10,31 @@ for p in spliter(str, ',') do
   actual[#actual + 1] = p
 end
 t.same(actual, expected, 'Build up a table of results from the split iterator')
+
+str = 'a    foo bar	bizz		buzz  b'
+expected = { 'a', 'foo', 'bar', 'bizz', 'buzz', 'b' }
+actual = {}
+for p in spliter(str) do
+  actual[#actual + 1] = p
+end
+t.same(actual, expected, 'Use iterator with nil pattern')
+
+str = 'foo'
+expected = { 'f', 'o', 'o' }
+actual = {}
+for p in spliter(str, '') do
+  print(#actual+1, p)
+  actual[#actual + 1] = p
+end
+t.same(actual, expected, 'Use iterator to explode a string')
+
+str = 'f.o.o'
+expected = { 'f', 'o', 'o' }
+actual = {}
+for p in spliter(str, '%.') do
+  actual[#actual + 1] = p
+end
+t.same(actual, expected, 'Use iterator with \'%.\' as the pattern')
 
 str = ',foo,bar,bizz,buzz,,'
 expected = { 'zzz', 'zzzfoo', 'zzzbar', 'zzzbizz', 'zzzbuzz', 'zzz', 'zzz' }
